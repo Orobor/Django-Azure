f4bdb3bc9833b2b395828e7f4543770292e92743
Orobor <orobor.ise@gmail.com>
2024-11-01 10:14:29 +0000

    modify hostname in deployment.py file

--- azure_project/deployment.py
+++ azure_project/deployment.py
@@ -4,7 +4,8 @@ from .settings import BASE_DIR
 
 
 SECRET_KEY = os.environ['SECRET']
-ALLOWED_HOSTS = ["https://objectdetectionapp-cmh7czdde4f2dwa2.canadacentral-01.azurewebsites.net/"]
+ALLOWED_HOSTS = ["https://objectdetectionapp-cmh7czdde4f2dwa2.canadacentral-01.azurewebsites.net"]
+CSRF_TRUSTED_ORIGINS = ["https://objectdetectionapp-cmh7czdde4f2dwa2.canadacentral-01.azurewebsites.net"]
 #ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 #CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
